c9dc6f4a348e7081331294f81cf0d5250e24c0ae
ihsanbaihaqii <baihaqiihsan31@gmail.com>
2026-08-05 13:13:39 +0700

    merge: scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,27 @@ import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 
+import ScrollToHash from "./components/ScrollToHash";
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="produk/:slug" element={<ProductDetail />} />
-        {/* User wanted custom routes like /manfaat/:slug, we can support both by routing to the same component */}
-        <Route
-          path="manfaat/:slug"
-          element={<ProductDetail focus="manfaat" />}
-        />
-        <Route
-          path="komposisi/:slug"
-          element={<ProductDetail focus="komposisi" />}
-        />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToHash />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="produk/:slug" element={<ProductDetail />} />
+          {/* User wanted custom routes like /manfaat/:slug, we can support both by routing to the same component */}
+          <Route
+            path="manfaat/:slug"
+            element={<ProductDetail focus="manfaat" />}
+          />
+          <Route
+            path="komposisi/:slug"
+            element={<ProductDetail focus="komposisi" />}
+          />
+        </Route>
+      </Routes>
+    </>
   );
 }
