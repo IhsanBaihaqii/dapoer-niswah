3481795d807398ecf2970c66c592fa4c6d95de39
ihsanbaihaqii <baihaqiihsan31@gmail.com>
2026-08-08 23:10:16 +0700

    add: api produk

--- api/mm.js
+++
@@ -1,5 +0,0 @@
-import { products } from "../src/data/products.js";
-
-export default function handler(req, res) {
-  res.status(200).json(products);
-}
--- api/produk.js
+++ api/produk.js
@@ -1,49 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import { products } from "../src/data/products.js";
 
-async function loadProduk(ukuran) {
-  try {
-    const filePath = path.join(
-      process.cwd(),
-      "assets",
-      "data",
-      "jamu",
-      `${ukuran}.json`,
-    );
-
-    const data = fs.readFileSync(filePath, "utf-8");
-
-    return JSON.parse(data);
-  } catch (error) {
-    return [];
-  }
+export default function handler(req, res) {
+  res.status(200).json(products);
 }
-
-async function getAllProduk() {
-  const results = await Promise.all([
-    loadProduk("60ml"),
-    loadProduk("250ml"),
-    loadProduk("500ml"),
-    loadProduk("1000ml"),
-  ]);
-
-  return results.flat();
-}
-
-module.exports = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  try {
-    const data = await getAllProduk();
-
-    res.status(200).json({
-      success: true,
-      jamu: { total: data.length, data },
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      error: err.message,
-    });
-  }
-};
